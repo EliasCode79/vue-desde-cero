32b2ba5e58927938026f3f5ef25c6dfe4cbf3c64
EliasCode79 <eliasfranco2379@gmail.com>
2023-08-13 21:50:07 -0400

    proyecto base para el game-pokemon

---
+++ 04-pokemon-game/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<img class="fade-in" alt="Vue logo" src="./assets/logo.png" />
+	<!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+	<PokemonPages />
+</template>
+
+<script>
+import PokemonPages from './pages/PokemonPages';
+
+export default {
+	name: 'App',
+	components: {
+		PokemonPages,
+	},
+};
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ 04-pokemon-game/src/pages/PokemonPages.vue
@@ -0,0 +1,20 @@
+<template>
+	<h1>Quien es este pokemon</h1>
+
+	<!-- TODO imagen aqui -->
+	<PokemonPictures />
+	<!-- TODO	opciones -->
+	<PokemonOptions />
+</template>
+
+<script>
+import PokemonPictures from '@/components/PokemonPictures';
+import PokemonOptions from '@/components/PokemonOptions';
+
+export default {
+	components: {
+		PokemonOptions,
+		PokemonPictures,
+	},
+};
+</script>
